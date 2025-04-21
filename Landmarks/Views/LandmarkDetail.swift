97e88de42dfa498fdbcd7575c044867355b1f945
Elou <ea.parquer@gmail.com>
2025-04-21 15:43:04 +0200

    Add list and navigation

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Elodie-Anne Parquer on 21/04/2025.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}

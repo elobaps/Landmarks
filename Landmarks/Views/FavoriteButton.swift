323122e93eeeb3cc0d184383a1d92f627f56a679
Elou <ea.parquer@gmail.com>
2025-04-21 21:28:38 +0200

    Filter Favorite with toggleButton and modelData with @Observable

---
+++ Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Elodie-Anne Parquer on 21/04/2025.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
---
+++ Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,31 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Elodie-Anne Parquer on 21/04/2025.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+//    @Binding permet à une vue enfant de modifier une donnée qui est détenue par une vue parent.
+//    C’est un pont entre parent et enfant pour partager une valeur modifiable.
+//    @State      Créer une donnée locale et réactive
+//    @Binding    Accéder/modifier une donnée créée ailleurs
+//    $variable   Créer une liaison (binding) vers une @State
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+//        .constant(true) Crée une valeur fixe juste pour la preview
+}
